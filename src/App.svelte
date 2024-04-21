59ed0f9293dcab5be05fa3a4db7d4d58ec88c0d9
Helge Sverre <helge.sverre@gmail.com>
2024-04-21 12:45:40 +0200

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-  import {
-    ChevronDown,
-    Drum,
-    Pause,
-    Play,
-    Search,
-    Soup,
-    Square,
-    ZoomIn,
-    ZoomOut,
-  } from "lucide-svelte";
+  import { ChevronDown, Drum, Pause, Play, Search, Square, ZoomIn, ZoomOut } from "lucide-svelte";
   import Track from "./lib/ui/Track.svelte";
 
   import {
     bpm,
-    playbackState,
-    zoomLevel,
-    timeToPixels,
-    tracks,
-    createDummyTracks,
+    createDummyDrumTracks,
+    createDummyDubstepTracks,
+    createDummyTranceTracks,
     pausePlayback,
-    loadDefaultTracks,
+    playbackState,
     startPlayback,
     stopPlayback,
+    timeToPixels,
+    tracks,
     zoomByDelta,
     zoomIn,
+    zoomLevel,
     zoomOut,
   } from "./core/store.js";
   import { formatTime } from "./core/audio.js";
@@ -43,7 +34,7 @@
 
   onMount(() => {
     if ($tracks.length === 0) {
-      createDummyTracks();
+      createDummyDrumTracks();
     }
   });
 
@@ -53,43 +44,32 @@
 <main>
   <div>
     <div
-      class="flex h-full w-full items-center justify-between border-b-2 border-dark-900 bg-dark-300 bg-gradient-to-t from-dark-400 via-dark-300 to-dark-300 px-2 py-2"
-    >
+      class="flex h-full w-full items-center justify-between border-b-2 border-dark-900 bg-dark-300 bg-gradient-to-t from-dark-400 via-dark-300 to-dark-300 px-2 py-2">
       <div class="text-xs font-medium leading-none text-white/40">H-DAW</div>
       <div class="text-xs font-medium leading-none text-white/40">v0.0.1</div>
     </div>
   </div>
   <section class="bg-dark-600">
     <div class="flex h-14 flex-row items-center">
-      <div
-        class="flex h-full w-[250px] shrink-0 items-center border-r-2 border-dark-900 px-2"
-      >
+      <div class="flex h-full w-[250px] shrink-0 items-center border-r-2 border-dark-900 px-2">
         <div class="relative h-fit w-full rounded bg-dark-400">
-          <div
-            class="absolute inset-y-0 left-2 flex items-center justify-center"
-          >
+          <div class="absolute inset-y-0 left-2 flex items-center justify-center">
             <Search size="16" class="text-light-soft" />
           </div>
 
           <input
             type="search"
             placeholder="Instruments, effects audio..."
-            class="h-10 w-full text-ellipsis bg-transparent pl-8 text-sm font-normal placeholder-light-secondary focus:border-transparent focus:outline-none focus:ring-0 focus:ring-transparent"
-          />
+            class="h-10 w-full text-ellipsis bg-transparent pl-8 text-sm font-normal placeholder-light-secondary focus:border-transparent focus:outline-none focus:ring-0 focus:ring-transparent" />
         </div>
       </div>
 
-      <div
-        class="flex w-full flex-row items-center justify-start gap-x-3 px-2 py-2"
-      >
-        <button
-          class="group inline-flex flex-row gap-1 overflow-hidden rounded"
-        >
+      <div class="flex w-full flex-row items-center justify-start gap-x-3 px-2 py-2">
+        <button class="group inline-flex flex-row gap-1 overflow-hidden rounded">
           <TextDisplay text="4/4" />
 
           <span
-            class="inline-flex h-10 items-center justify-center bg-dark-400 px-1 font-bold text-light group-hover:bg-dark-200"
-          >
+            class="inline-flex h-10 items-center justify-center bg-dark-400 px-1 font-bold text-light group-hover:bg-dark-200">
             <ChevronDown size="20" />
           </span>
         </button>
@@ -120,16 +100,14 @@
 
         <div class="ml-auto">
           <SegmentGroup additionalClasses="mx-8">
-            <IconButton icon={Drum} onClick={createDummyTracks} />
-            <IconButton icon={Soup} onClick={loadDefaultTracks} />
+            <IconButton icon={Drum} onClick={createDummyDrumTracks} />
+            <IconButton icon={Drum} onClick={createDummyDubstepTracks} />
+            <IconButton icon={Drum} onClick={createDummyTranceTracks} />
           </SegmentGroup>
 
           <SegmentGroup>
             <IconButton icon={ZoomIn} onClick={zoomIn} />
-            <TextDisplay
-              text={`${Math.round($zoomLevel)}%`}
-              additionalClasses="w-14 text-center select-none"
-            />
+            <TextDisplay text={`${Math.round($zoomLevel)}%`} additionalClasses="w-14 text-center select-none" />
             <IconButton icon={ZoomOut} onClick={zoomOut} />
           </SegmentGroup>
         </div>
@@ -139,10 +117,7 @@
 
   <!-- Timeline -->
   <section class="timeline relative" on:wheel={handleZoom}>
-    <div
-      style="left: {playHeadPosition}px;"
-      class="absolute inset-y-0 z-50 ml-[250px] w-[2px] bg-red-500"
-    ></div>
+    <div style="left: {playHeadPosition}px;" class="absolute inset-y-0 z-50 ml-[250px] w-[2px] bg-red-500"></div>
 
     <!-- Tracks -->
     <div class="flex flex-col gap-1 p-2">
