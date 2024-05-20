690ced676ebfed3c779c996ef9a84cb65a90d1f7
Helge Sverre <helge.sverre@gmail.com>
2024-05-20 06:55:53 +0200

    adsr editor

--- src/lib/daw/MixerPanel.svelte
+++ src/lib/daw/MixerPanel.svelte
@@ -1,16 +1,19 @@
 <script>
   import { tweened } from "svelte/motion";
-  import { cubicInOut, elasticInOut } from "svelte/easing";
+  import { cubicInOut } from "svelte/easing";
   import Fader from "../ui/Fader.svelte";
   import { masterPan, masterVolume } from "../../core/store.js";
   import { ChevronUp } from "lucide-svelte";
+  import { onMount } from "svelte";
+  import { audioManager } from "../../core/audio.js";
+  import ADSR from "../ui/ADSR.svelte";
 
   const openHeight = 350;
   const closedHeight = 30;
 
-  let isMixerOpen = false;
+  let isMixerOpen = true;
   const mixerHeight = tweened(isMixerOpen ? openHeight : closedHeight, {
-    duration: 100,
+    duration: 50,
     easing: cubicInOut,
   });
 
@@ -45,6 +48,43 @@
       toggleMixer();
     }
   }
+
+  let peakMeter = 0;
+
+  onMount(() => {
+    let analyser = audioManager.audioContext.createAnalyser();
+    audioManager.mixer.connect(analyser);
+
+    function updateSoundLevel() {
+      // Create an array to hold the time domain data
+      let data = new Uint8Array(analyser.fftSize);
+
+      analyser.getByteTimeDomainData(data);
+
+      // Calculate the RMS of the data
+      let sum = 0;
+      for (let i = 0; i < data.length; i++) {
+        const value = (data[i] - 128) / 128;
+        sum += value * value;
+      }
+      const rms = Math.sqrt(sum / data.length);
+
+      // Convert the RMS to dB
+      let db = 20 * Math.log10(rms);
+
+      const minDb = -60;
+      const maxDb = 0;
+      peakMeter = (100 * (db - minDb)) / (maxDb - minDb);
+
+      if (peakMeter < 0) {
+        peakMeter = 0;
+      }
+
+      requestAnimationFrame(updateSoundLevel);
+    }
+
+    updateSoundLevel();
+  });
 </script>
 
 <svelte:window on:keyup={handleKeyup} />
@@ -56,38 +96,55 @@
       Mixer
     </button>
   </div>
-  <div aria-hidden="true" class="handle" on:mousedown={startResizing}></div>
-  <div class="flex h-full flex-row items-start justify-between bg-dark-500 p-4">
-    <section class=" w-20 overflow-hidden rounded border border-dark-100 bg-dark-300">
+  <div aria-hidden="true" class="h-2 w-full cursor-row-resize bg-dark-100" on:mousedown={startResizing}></div>
+  <div class="flex h-full flex-row items-start justify-start gap-6 border-2 border-dark-100 bg-dark-600 p-4">
+    <div class="flex flex-col overflow-hidden rounded border border-dark-100 bg-dark-300">
       <div class="flex w-full flex-col items-center justify-center border-b border-dark-100 bg-dark-200 p-1">
         <div class="text-sm font-normal text-white">Master</div>
       </div>
-      <div class="flex flex-col gap-2">
-        <div
-          class="flex flex-col items-center justify-center border-b border-dark-200 bg-dark-600 px-1 py-1 py-2 text-center"
-        >
-          <input
-            bind:value={$masterPan}
-            type="range"
-            min="-1"
-            max="1"
-            step="0.1"
-            class="bg-dark-40 block w-full cursor-pointer rounded border border-red-400"
-          />
-          <span class="mt-1 text-center text-xs font-medium tabular-nums text-gray-400">{$masterPan}</span>
-          <span class="text-xs text-gray-200">Pan</span>
-        </div>
+      <div class="flex h-full flex-1 flex-row">
+        <div class="h-full w-20">
+          <div class="flex flex-col gap-2">
+            <div
+              class="flex flex-col items-center justify-center border-b border-dark-200 bg-dark-600 px-1 py-2 text-center"
+            >
+              <input
+                bind:value={$masterPan}
+                type="range"
+                min="-1"
+                max="1"
+                step="0.1"
+                class="bg-dark-40 block w-full cursor-pointer rounded border border-red-400"
+              />
+              <span class="mt-1 text-center text-xs font-medium tabular-nums text-gray-400">{$masterPan}</span>
+              <span class="text-xs text-gray-200">Pan</span>
+            </div>
 
-        <div class="flex flex-col items-center justify-center py-1 text-center">
-          <Fader bind:volume={$masterVolume} />
+            <div class="flex flex-col items-center justify-center py-1 text-center">
+              <Fader bind:volume={$masterVolume} />
 
-          <div class="text-center text-xs font-medium tabular-nums text-gray-400">
-            {Math.round($masterVolume * 100)}%
+              <div class="text-center text-xs font-medium tabular-nums text-gray-400">
+                {Math.round($masterVolume * 100)}%
+              </div>
+              <span class="text-xs text-gray-200">Volume</span>
+            </div>
           </div>
-          <span class="text-xs text-gray-200">Volume</span>
+        </div>
+        <div class="flex w-6 flex-1 flex-row items-stretch justify-stretch border-l border-dark-100">
+          <div class="relative h-full w-full bg-gradient-to-t from-green-600 via-yellow-600 to-red-700 contrast-50">
+            <span class="absolute inset-x-0 top-4 text-center text-xs">{peakMeter.toFixed(0)}</span>
+            <div
+              class="absolute inset-x-0 bottom-0 border-t-4 border-red-700 backdrop-contrast-200"
+              style={`min-height: ${peakMeter}%`}
+            ></div>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
+
+    <div>
+      <ADSR />
+    </div>
   </div>
 </div>
 
@@ -100,12 +157,5 @@
     left: 0;
     right: 0;
     z-index: 1000;
-  }
-
-  .handle {
-    width: 100%;
-    height: 6px;
-    background-color: var(--dark-100);
-    cursor: row-resize;
   }
 </style>
---
+++ src/lib/ui/ADSR.svelte
@@ -0,0 +1,204 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let amount = 0.8;
+  export let attack = 0.1;
+  export let decay = 0.1;
+  export let sustain = 0.5;
+  export let release = 0.5;
+
+  export let width = 350;
+  export let height = 150;
+
+  $: attackX = attack * width;
+  $: decayX = attackX + decay * width;
+  $: sustainY = height - sustain * height;
+  $: sustainX = width - release * width;
+
+  let svg;
+  let draggingPoint = null;
+
+  function startDrag(point) {
+    draggingPoint = point;
+  }
+
+  function stopDrag() {
+    draggingPoint = null;
+  }
+
+  function drag(event) {
+    if (draggingPoint) {
+      const rect = svg.getBoundingClientRect();
+      const x = ((event.clientX - rect.left) / rect.width) * width;
+      const y = ((event.clientY - rect.top) / rect.height) * height;
+
+      switch (draggingPoint) {
+        case "attack":
+          attack = Math.max(0, Math.min(1, x / width));
+          break;
+        case "decay":
+          decay = Math.max(0, Math.min(1, (x - attackX) / width));
+          break;
+        case "sustain":
+          sustain = Math.max(0, Math.min(1, 1 - y / height));
+          break;
+        case "release":
+          release = Math.max(0, Math.min(1, (width - x) / width));
+          break;
+      }
+    }
+  }
+
+  onMount(() => {
+    window.addEventListener("mouseup", stopDrag);
+    window.addEventListener("mousemove", drag);
+  });
+</script>
+
+<div class="flex flex-col rounded border border-dark-100 bg-dark-700 p-2">
+  <div class="mb-2 flex w-full flex-row gap-2">
+    <div class="flex flex-col gap-1">
+      <label class="text-xs text-white/50">Attack</label>
+      <input
+        class="w-full appearance-none border border-dark-200 bg-dark-700 p-0 text-center text-xs text-white"
+        type="number"
+        min="0.0"
+        max="1"
+        step="0.01"
+        bind:value={attack}
+      />
+    </div>
+    <div class="flex flex-col gap-1">
+      <label class="text-xs text-white/50">Decay</label>
+      <input
+        class="w-full appearance-none border border-dark-200 bg-dark-700 p-0 text-center text-xs text-white"
+        type="number"
+        min="0.0"
+        max="1"
+        step="0.01"
+        bind:value={decay}
+      />
+    </div>
+    <div class="flex flex-col gap-1">
+      <label class="text-xs text-white/50">Sustain</label>
+      <input
+        class="w-full appearance-none border border-dark-200 bg-dark-700 p-0 text-center text-xs text-white"
+        type="number"
+        min="0.0"
+        max="1"
+        step="0.01"
+        bind:value={sustain}
+      />
+    </div>
+    <div class="flex flex-col gap-1">
+      <label class="text-xs text-white/50">Release</label>
+      <input
+        class="w-full appearance-none border border-dark-200 bg-dark-700 p-0 text-center text-xs text-white"
+        type="number"
+        min="0.0"
+        max="1"
+        step="0.01"
+        bind:value={release}
+      />
+    </div>
+  </div>
+  <svg bind:this={svg} overflow="visible" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
+    <!-- Border -->
+    <path
+      d="M 0 {height} L 0 0 L {width} 0 L {width} {height} Z"
+      fill="rgba(0,0,0,0.5)"
+      stroke="rgba(255,255,255,0.25)"
+      stroke-width="1"
+    />
+
+    <!-- Grid lines marking each stage -->
+    <line
+      x1={attackX}
+      y1="0"
+      x2={attackX}
+      y2={height}
+      stroke="rgba(255,255,255,0.2)"
+      stroke-width="2"
+      stroke-dasharray="4"
+    />
+
+    <line
+      x1={decayX}
+      y1="0"
+      x2={decayX}
+      y2={height}
+      stroke="rgba(255,255,255,0.2)"
+      stroke-width="2"
+      stroke-dasharray="4"
+    />
+    <line
+      x1={sustainX}
+      y1="0"
+      x2={sustainX}
+      y2={height}
+      stroke="rgba(255,255,255,0.2)"
+      stroke-width="2"
+      stroke-dasharray="4"
+    />
+
+    <!-- Envelope Curve -->
+    <path
+      d={`M 0 ${height} L ${attackX} 0 L ${decayX} ${sustainY} H ${sustainX} L ${width} ${height} Z`}
+      fill="rgba(255, 255, 255, 0.5)"
+      stroke="white"
+      stroke-width="1"
+    />
+
+    <!-- Attack Control Point -->
+    <circle
+      aria-hidden="true"
+      cx={attackX}
+      cy="0"
+      r="5"
+      stroke-width="1"
+      stroke="white"
+      fill="silver"
+      class="cursor-pointer"
+      on:mousedown={() => startDrag("attack")}
+    />
+
+    <!-- Decay Control Point -->
+    <circle
+      aria-hidden="true"
+      cx={decayX}
+      cy={sustainY}
+      r="5"
+      stroke-width="1"
+      stroke="white"
+      fill="silver"
+      class="cursor-pointer"
+      on:mousedown={() => startDrag("decay")}
+    />
+
+    <!-- Sustain Control Point -->
+    <circle
+      aria-hidden="true"
+      cx={sustainX}
+      cy={sustainY}
+      r="5"
+      stroke-width="1"
+      stroke="white"
+      fill="silver"
+      class="cursor-pointer"
+      on:mousedown={() => startDrag("sustain")}
+    />
+
+    <!-- Release Control Point -->
+    <circle
+      aria-hidden="true"
+      cx={width}
+      cy={height}
+      r="5"
+      stroke-width="1"
+      stroke="white"
+      fill="silver"
+      class="cursor-pointer"
+      on:mousedown={() => startDrag("release")}
+    />
+  </svg>
+</div>
