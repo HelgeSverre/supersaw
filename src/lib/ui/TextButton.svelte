a643a291cdf2ecc403b2c4d17e282d5c0b409624
Helge Sverre <helge.sverre@gmail.com>
2024-05-31 23:48:06 +0200

    buttons should never wrap

--- src/lib/ui/TextButton.svelte
+++ src/lib/ui/TextButton.svelte
@@ -6,7 +6,7 @@
 
 <button
   on:click={onClick}
-  class={`inline-flex h-10 items-center justify-center rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light hover:bg-dark-200 ${additionalClasses}`}
+  class={`inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light hover:bg-dark-200 ${additionalClasses}`}
 >
   {text}
 </button>
