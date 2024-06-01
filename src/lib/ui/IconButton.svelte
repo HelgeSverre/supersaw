09ce1f7c7a09e16a90242124756c9ba86c893254
Helge Sverre <helge.sverre@gmail.com>
2024-06-01 02:26:52 +0200

    wip

--- src/lib/ui/IconButton.svelte
+++ src/lib/ui/IconButton.svelte
@@ -1,4 +1,6 @@
 <script>
+  import classNames from "classnames";
+
   export let onClick = () => {};
   export let icon;
   export let iconSize = 18;
@@ -7,7 +9,11 @@
 
 <button
   on:click={onClick}
-  class={`inline-flex size-10 items-center justify-center rounded bg-dark-400 leading-none text-light hover:bg-dark-200 ${additionalClasses}`}
+  class={classNames(
+    "inline-flex size-10 items-center justify-center rounded bg-dark-400 leading-none text-light",
+    "hover:bg-dark-200 focus:outline-none focus:ring-1 focus:ring-dark-100",
+    additionalClasses,
+  )}
 >
   {#if icon}
     <svelte:component this={icon} size={iconSize} />
--- src/lib/ui/TextButton.svelte
+++ src/lib/ui/TextButton.svelte
@@ -1,4 +1,6 @@
 <script>
+  import classNames from "classnames";
+
   export let onClick = () => {};
   export let additionalClasses = "";
   export let text;
@@ -6,7 +8,11 @@
 
 <button
   on:click={onClick}
-  class={`inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light hover:bg-dark-200 ${additionalClasses}`}
+  class={classNames(
+    "inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light",
+    "hover:bg-dark-200 focus:outline-none focus:ring-1 focus:ring-dark-100",
+    additionalClasses,
+  )}
 >
   {text}
 </button>
