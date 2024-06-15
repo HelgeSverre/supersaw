b856f5cf2eb019c67b2e8956d5d946d4554540e9
Helge Sverre <helge.sverre@gmail.com>
2024-06-15 16:36:10 +0200

    wip

--- src/core/effects/compressor.ts
+++ src/core/effects/compressor.ts
@@ -8,10 +8,10 @@ export class Compressor implements Effect {
     this.audioContext = audioContext;
     this.compressor = this.audioContext.createDynamicsCompressor();
     this.compressor.threshold.setValueAtTime(-6, this.audioContext.currentTime);
-    this.compressor.knee.setValueAtTime(10, this.audioContext.currentTime);
+    this.compressor.knee.setValueAtTime(12, this.audioContext.currentTime);
     this.compressor.ratio.setValueAtTime(4, this.audioContext.currentTime);
     this.compressor.attack.setValueAtTime(0.1, this.audioContext.currentTime);
-    this.compressor.release.setValueAtTime(0.5, this.audioContext.currentTime);
+    this.compressor.release.setValueAtTime(0.8, this.audioContext.currentTime);
   }
 
   connect(node: AudioNode) {
