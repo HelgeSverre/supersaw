dbf58329ad641382553fe1035d291b8eed4504a9
Helge Sverre <helge.sverre@gmail.com>
2024-06-19 07:21:37 +0200

    better

--- src/utils/generators/wip.js
+++ src/utils/generators/wip.js
@@ -4,7 +4,7 @@ export class GeneratorHardstyle {
     this.beatDuration = (60 / this.bpm) * 1000; // Duration of one beat in milliseconds
     this.barLength = 4 * this.beatDuration; // Duration of one bar in milliseconds
     this.notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
-    this.root = "A"; // The root note from which the scale is generated
+    this.scaleType = "harmonicMinor";
   }
 
   getScaleIntervals(scaleType = "harmonicMinor") {
@@ -42,47 +42,76 @@ export class GeneratorHardstyle {
     const basslineInfo = this.generateBassline(bars);
     const { bassline, tonics } = basslineInfo;
 
-    const melody = this.generateMelody(bars, bassline, tonics);
+    const melody = this.generateMelody(bars, tonics);
     return { melody, bassline, tonics };
   }
 
-  generateMelody(bars = 4, bassline, tonics) {
+  // createStructuredMotif(scaleNotes) {
+  //   const motif = [];
+  //
+  //   // Define a simple pattern for the motif
+  //   const pattern = [1, 2, 4, 5, 7, 9, 10, 12, 14, 15, 17, 19, 20, 22, 23, 24];
+  //
+  //   for (let i = 0; i < 8; i++) {
+  //     const index = pattern[i];
+  //     const note = scaleNotes[index % scaleNotes.length];
+  //     motif.push(note);
+  //   }
+  //
+  //   return motif;
+  // }
+
+  createStructuredMotif(scaleNotes) {
+    const motif = [];
+
+    // Generate a random pattern for the motif
+    const pattern = [];
+    for (let i = 0; i < 16; i++) {
+      pattern.push(Math.floor(Math.random() * scaleNotes.length));
+    }
+
+    for (let i = 0; i < 16; i++) {
+      const index = pattern[i];
+      const note = scaleNotes[index];
+      motif.push(note);
+    }
+
+    return motif;
+  }
+
+  generateMelody(bars = 4, tonics) {
     const melody = [];
 
     for (let bar = 0; bar < bars; bar++) {
       const tonic = tonics[bar];
       const scaleNotes = this.getScaleNotes(tonic, this.scaleType);
-
-      let currentTime = bar * this.barLength;
+      const motif = this.createStructuredMotif(scaleNotes);
 
       for (let beat = 0; beat < 4; beat++) {
         const startTime = bar * this.barLength + beat * this.beatDuration;
-        const duration = this.beatDuration * 0.7;
-        const note = scaleNotes[Math.floor(Math.random() * scaleNotes.length)];
-        const noteDuration = this.beatDuration * 0.5; // Half beat note duration
+        const duration = this.beatDuration * 0.25;
+        const noteIndex = beat * 4 + Math.floor(Math.random() * 4);
+        const note = motif[noteIndex];
 
-        melody.push({
-          color: "yellow",
-          frequency: this.noteToFrequency(note),
-          startTime: startTime,
-          duration: duration,
-        });
-
-        currentTime += noteDuration;
+        if (note) {
+          melody.push({
+            color: "yellow",
+            frequency: this.noteToFrequency(note),
+            startTime: startTime,
+            duration: duration,
+          });
+        }
       }
     }
     return melody;
   }
 
   generateBassline(bars = 4) {
-    const scaleNotes = this.getScaleNotes(this.root, this.scaleType);
-
     const bassline = [];
-    const tonics = [];
+    const tonics = ["G", "G", "C", "C", "E", "E", "D", "E"];
 
     for (let bar = 0; bar < bars; bar++) {
-      const tonic = scaleNotes[Math.floor(Math.random() * scaleNotes.length)];
-      tonics.push(tonic);
+      const tonic = tonics[bar];
       const baseNoteFrequency = this.noteToFrequency(tonic);
 
       for (let beat = 0; beat < 4; beat++) {
@@ -108,32 +137,4 @@ export class GeneratorHardstyle {
 
     return { bassline, tonics };
   }
-
-  // generateHardstyleBassline(melody, bars = 4) {
-  //   const bassline = [];
-  //   for (let bar = 0; bar < bars; bar++) {
-  //     const baseNoteFrequency = melody.length > 0 ? melody[0].frequency : 130.81; // Default or first note
-  //
-  //     for (let beat = 0; beat < 4; beat++) {
-  //       const startTime = bar * this.barLength + beat * this.beatDuration;
-  //       const highDuration = this.beatDuration * 0.75;
-  //       const lowDuration = this.beatDuration * 0.25;
-  //
-  //       bassline.push({
-  //         color: "red",
-  //         frequency: baseNoteFrequency, // High part of the bass
-  //         duration: highDuration,
-  //         startTime: startTime,
-  //       });
-  //
-  //       bassline.push({
-  //         color: "blue",
-  //         frequency: baseNoteFrequency / 2, // Low part of the bass, one octave down
-  //         duration: lowDuration,
-  //         startTime: startTime + highDuration,
-  //       });
-  //     }
-  //   }
-  //   return bassline;
-  // }
 }
