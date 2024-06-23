d106de4d38072cbd79752f0b3e2577c27eb8ade1
Helge Sverre <helge.sverre@gmail.com>
2024-06-23 02:18:34 +0200

    support on:click in button component

--- src/lib/ui/IconButton.svelte
+++ src/lib/ui/IconButton.svelte
@@ -1,14 +1,22 @@
 <script>
   import classNames from "classnames";
+  import { createEventDispatcher } from "svelte";
 
   export let onClick = () => {};
   export let icon;
   export let iconSize = 18;
   export let additionalClasses = "";
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick(event) {
+    onClick(event);
+    dispatch("click", event);
+  }
 </script>
 
 <button
-  on:click={onClick}
+  on:click={handleClick}
   class={classNames(
     "inline-flex size-10 items-center justify-center rounded bg-dark-400 leading-none text-light",
     "hover:bg-dark-200 focus:outline-none focus:ring-1 focus:ring-dark-100",
--- src/lib/ui/TextButton.svelte
+++ src/lib/ui/TextButton.svelte
@@ -1,18 +1,28 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import classNames from "classnames";
 
   export let onClick = () => {};
   export let additionalClasses = "";
-  export let text;
+  export let text = null;
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick(event) {
+    onClick(event);
+    dispatch("click", event);
+  }
 </script>
 
 <button
-  on:click={onClick}
+  on:click={handleClick}
   class={classNames(
-    "inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light",
+    "inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-dark-400 px-3 text-sm font-normal leading-none text-light",
     "hover:bg-dark-200 focus:outline-none focus:ring-1 focus:ring-dark-100",
     additionalClasses,
   )}
 >
-  {text}
+  <slot>
+    {text}
+  </slot>
 </button>
